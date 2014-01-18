832155431de3c94fd3719ec625cbea94ad26d1c8
Alexander Taylor <alexanderjohntaylor@gmail.com>
2014-01-18 19:19:36 +0000

    Initial commit (just displays board)

---
+++ main.py
@@ -0,0 +1,96 @@
+from kivy.app import App
+from kivy.vector import Vector
+
+from kivy.uix.widget import Widget
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.relativelayout import RelativeLayout
+
+from kivy.properties import (NumericProperty, ListProperty,
+                             ReferenceListProperty, StringProperty,
+                             BooleanProperty, ObjectProperty)
+
+
+class BoardContainer(AnchorLayout):
+    board = ObjectProperty()
+
+class Board(Widget):
+
+    grid_x = NumericProperty(15)
+    grid_y = NumericProperty(19)
+    grid = ReferenceListProperty(grid_x, grid_y)
+    
+    shape_x = NumericProperty(15)
+    shape_y = NumericProperty(19)
+    shape = ReferenceListProperty(shape_x, shape_y)
+
+    padding_x = NumericProperty(0)
+    padding_y = NumericProperty(0)
+    padding = ReferenceListProperty(padding_x, padding_y)
+
+    cell_size_x = NumericProperty()
+    cell_size_y = NumericProperty()
+    cell_size = ReferenceListProperty(cell_size_x, cell_size_y)
+
+    portrait = BooleanProperty()  # True if shape_x < shape_y
+    aspect_ratio = NumericProperty()
+
+    board_image = StringProperty('boards/edphoto_section_light.png')
+
+    grid_points = ListProperty([])
+
+    def pos_to_coords(self, pos):
+        '''Takes a pos in screen coordinates, and converts to a grid
+        position.'''
+    
+    def calculate_lines(self, *args):
+        '''Calculates the points that should make up the board lines, and sets
+        self.grid_points appropriately.'''
+
+        pos = Vector(self.pos)
+        padding = Vector(self.padding)
+        shape = Vector(self.shape) 
+        grid = Vector(self.grid)
+        cell_size = Vector(self.cell_size)
+
+        # Initial offset
+        init_offset = cell_size / 2.
+
+        # grid corners
+        bl = pos + init_offset + padding*cell_size
+        br = bl + Vector((grid[0]-1) * cell_size[0], 0)
+        tr = bl + (grid-Vector(1,1)) * cell_size
+        tl = bl + Vector(0, (grid[1]-1) * cell_size[1])
+
+        points = [bl[0], bl[1], br[0], br[1], tr[0], tr[1], tl[0], tl[1], bl[0], bl[1]]
+
+        cur_pos = bl
+        dir = 1
+        for x in range(grid[0]-1):
+            cur_pos[0] += cell_size[0]
+            points.append(cur_pos[0])
+            points.append(cur_pos[1])
+            cur_pos[1] += (grid[1]-1) * cell_size[1] * dir
+            points.append(cur_pos[0])
+            points.append(cur_pos[1])
+            dir *= -1
+
+        ydir = -1
+        for y in range(grid[1]-1):
+            cur_pos[1] += cell_size[1] * dir
+            points.append(cur_pos[0])
+            points.append(cur_pos[1])
+            cur_pos[0] += (grid[0]-1) * cell_size[0] * ydir
+            points.append(cur_pos[0])
+            points.append(cur_pos[1])
+            ydir *= -1
+
+        self.grid_points = points
+
+
+
+class PhutballApp(App):
+    def build(self):
+        return BoardContainer()
+
+if __name__ == "__main__":
+    PhutballApp().run()
