768c149b20a7d76cbe380d0302bf61f8557f93d1
Alexander Taylor <alexanderjohntaylor@gmail.com>
2014-02-21 23:40:39 +0000

    Added homescreen and back handling

--- interface.py
+++ interface.py
@@ -1,9 +1,47 @@
 '''Widgets for the full app interface.'''
 
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
+from kivy.properties import ListProperty
 
 class PhutballManager(ScreenManager):
-    pass
+    previous = ListProperty([])
+    def new_board(self, ai=True, from_file=None, ):
+        '''Creates and moves to a new board screen.'''
+        if not self.has_screen('board'):
+            new_screen = GameScreen(name='board')
+            self.add_widget(new_screen)
+        self.current = 'board'
 
-class AIGameScreen(Screen):
+    def on_current(self, *args):
+        super(PhutballManager, self).on_current(*args)
+        current = self.current
+        previous = self.previous
+        if current in previous:
+            print 'current in previous'
+            self.previous = previous[:previous.index(current)]
+        else:
+            previous.append(current)
+
+    def go_back(self, *args):
+        if self.transition.is_active:
+            return
+        self.transition = SlideTransition(direction='right')
+        if len(self.previous) < 2:
+            self.previous = []
+            self.current = 'home'
+            return
+        self.previous.pop()
+        new = self.previous[-1]
+        if new:
+            self.current = new
+        else:
+            self.current = 'home'
+        self.transition = SlideTransition(direction='left')
+        
+
+class GameScreen(Screen):
+    '''Screen containing a BoardInterface'''
+    
+
+class HomeScreen(Screen):
     pass
--- main.py
+++ main.py
@@ -2,11 +2,32 @@ from guiboard import BoardInterface
 from interface import PhutballManager
 
 from kivy.app import App
+from kivy.properties import ObjectProperty
+from kivy.core.window import Window
+from kivy.utils import platform
 
 
 class PhutballApp(App):
+    manager = ObjectProperty()
     def build(self):
-        return PhutballManager()
+        self.bind(on_start=self.post_build_init)
+        manager = PhutballManager()
+        self.manager = manager
+        return manager
+
+    def post_build_init(self,ev):
+        if platform() == 'android':
+            import android
+            android.map_key(android.KEYCODE_BACK, 1001)
+
+        win = Window
+        win.bind(on_keyboard=self.key_handler)
+
+    def key_handler(self, window, keycode1, keycode2, text, modifiers):
+        if keycode1 == 27 or keycode1 == 1001:
+            self.manager.go_back()
+            return True
+        return False
 
 
 if __name__ == "__main__":
